57d4d2bd763fd52609240e4dbafab1cc95e80379
YauhenKuntsevich <jauhien.kuncevicz@gmail.com>
2023-02-19 17:35:09 +0300

    Added script for Hangman

--- Assets/Scripts/HangmanGame.cs
+++ Assets/Scripts/HangmanGame.cs
@@ -1,50 +1,112 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
+using UnityEngine.WSA;
+using Random = UnityEngine.Random;
 
-public class HangmanGame : MonoBehaviour
+namespace Hangman.Scripts
 {
-    private List<string> guessedKeys = new List<string>();
-    
-    private KeyCode lastKeyCode;
-    
-    void OnGUI()
+    public class HangmanGame : MonoBehaviour
     {
-        Event e = Event.current;
-        if (e.isKey)
-        {
-            Debug.Log("Detected key code: " + e.keyCode);
-            
-            if (e.keyCode != KeyCode.None)
-            {
-                ProcessKey(e.keyCode);
+        [SerializeField] private TextMeshProUGUI _textField;
+        [SerializeField] private int hp = 7;
 
-                KeyCode lastKeyPressed = e.keyCode;
+        private List<char> guessedLetters = new List<char>();
+        private List<char> wrongTriedLetter = new List<char>();
+
+        private string[] words =
+        {
+            "Cat",
+            "Dog",
+            "Rain",
+            "Unity",
+            "Time"
+        };
+
+        private string wordToGuess = "";
+
+        private KeyCode lastKeyPressed;
+
+        private void Start()
+        {
+            var randomIndex = Random.Range(0, words.Length);
+
+            wordToGuess = words[randomIndex];
+        }
+
+
+        void OnGUI()
+        {
+            Event e = Event.current;
+            if (e.isKey)
+            {
+                // Debug.Log("Detected key code: " + e.keyCode);
+
+                if (e.keyCode != KeyCode.None && lastKeyPressed != e.keyCode)
+                {
+                    ProcessKey(e.keyCode);
+
+                    lastKeyPressed = e.keyCode;
+                }
             }
         }
-    }
 
-    private void ProcessKey(KeyCode key)
-    {
-        var word = "Time";
-
-        print("Word to guess is: " + word);
-
-        print("Key Pressed: " + key);
-
-        string keyString = key.ToString();
-
-        string wordUppercase = word.ToUpper;
-        bool wordContainsPressedKey = word.Contains(keyString);
-        bool letterWasGuessed = guessedKeys.Contains(keyString);
-
-        print ("wordContainePressedKey: " + wordContainsPressedKey);
-
-        if(wordContainsPressedKey && !letterWasGuessed)
+        private void ProcessKey(KeyCode key)
         {
-            guessedKeys.Add(keyString);
-        }
+            print("Key Pressed: " + key);
 
-        print(string.Join{", "}, guessedKeys);
+            char pressedKeyString = key.ToString()[0];
+
+            string wordUppercase = wordToGuess.ToUpper();
+
+            bool wordContainsPressedKey = wordUppercase.Contains(pressedKeyString);
+            bool letterWasGuessed = guessedLetters.Contains(pressedKeyString);
+
+            if (!wordContainsPressedKey && !wrongTriedLetter.Contains(pressedKeyString))
+            {
+                wrongTriedLetter.Add(pressedKeyString);
+                hp -= 1;
+
+                if (hp <= 0)
+                {
+                    print("You Lost!");
+                }
+                else
+                {
+                    print("Wrong letter! Hp left = " + hp);
+                }
+            }
+
+            if (wordContainsPressedKey && !letterWasGuessed)
+            {
+                guessedLetters.Add(pressedKeyString);
+            }
+
+            string stringToPrint = "";
+            for (int i = 0; i < wordUppercase.Length; i++)
+            {
+                char letterInWord = wordUppercase[i];
+
+                if (guessedLetters.Contains(letterInWord))
+                {
+                    stringToPrint += letterInWord;
+                }
+                else
+                {
+                    stringToPrint += "_";
+                }
+            }
+
+            if (wordUppercase == stringToPrint)
+            {
+                print("You win!");
+            }
+
+            // print(string.Join(", ", guessedLetters));
+            print(stringToPrint);
+            _textField.text = stringToPrint;
+        }
     }
 }
